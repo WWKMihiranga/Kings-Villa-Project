e69a2db00a7d52c707e44e0011c198e6bcc5508f
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-09-14 14:53:28 +0530

    Added div outside to the list

--- kings-villa-project/src/Components/Map/Map.js
+++ kings-villa-project/src/Components/Map/Map.js
@@ -4,13 +4,15 @@ function TextDisplay() {
   return (
     <>
       <h1>Distance Tracker</h1>
-      <h3>Nearest Beaches</h3>
-      <ul>
-        <li>Mahamodara Beach - 1.1km</li>
-        <li>Galle Fort Beach - 2.6km</li>
-        <li>Dadalla Beach - 2.3km</li>
-        <li>Unawatuna Beach - 4km</li>
-      </ul>
+      <div>
+        <h3>Nearest Beaches</h3>
+        <ul>
+          <li>Mahamodara Beach - 1.1km</li>
+          <li>Galle Fort Beach - 2.6km</li>
+          <li>Dadalla Beach - 2.3km</li>
+          <li>Unawatuna Beach - 4km</li>
+        </ul>
+      </div>
     </>
   );
 }
