a77b9d20a9c49cc46041a633b58bfef8b6116419
Kavindu Mihiranga <kavirangot@gmail.com>
2023-09-15 11:13:45 +0530

    Fixed some errors

--- kings-villa-project/src/App.js
+++ kings-villa-project/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Map from "./components/Map/Map";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <Map />;
+  return (
+  <>
+  <Map />
+  <Footer />
+  </>
+  )
 }
 
 export default App;
