29546e7c74779c9eedc7a1fa76b1f9492786e25f
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-09-17 18:38:30 +0530

    Change map image into a google map containing iframe

--- kings-villa-project/src/Components/Map/Map.js
+++ kings-villa-project/src/Components/Map/Map.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { GoogleMap, GoogleApiWrapper, Marker } from "@react-google-maps/api";
+import Iframe from "react-iframe";
 import MapImage from "./map_image.png";
 import "./map.css";
 
@@ -38,8 +40,17 @@ function TextDisplay() {
             </ul>
           </div>
         </div>
-        <div className="img_div col">
-          <img src={MapImage} alt="Map of Galle Subaurb" />
+        <div className="col img_div">
+          {/* <img src={MapImage} alt="Map of Galle Subaurb" /> */}
+          <Iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d816.3635848030631!2d80.20967182328074!3d6.047619288536783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae173e9c636b5f5%3A0xe1157d7a971600f0!2sKINGS%20VILLA!5e0!3m2!1sen!2ssg!4v1694950429073!5m2!1sen!2ssg"
+            width="600"
+            height="450"
+            style="border:0;"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></Iframe>
         </div>
       </div>
     </>
